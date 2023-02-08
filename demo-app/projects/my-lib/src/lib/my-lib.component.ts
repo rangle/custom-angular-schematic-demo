f698d34cb1e196b20f29c0b863e963da3c5434e4
Stephanie Zeng <stephanie.zeng@rangle.io>
2023-02-08 12:06:56 -0500

    Add demo Angular project

---
+++ demo-app/projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: `
+    <p>
+     my library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibComponent {
+
+}
---
+++ demo-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MyLibModule } from 'projects/my-lib/src/public-api';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    MyLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
