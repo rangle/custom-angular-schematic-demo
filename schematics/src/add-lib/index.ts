afad9c9cd6a339e70ce29465972bb6b26cfeebce
Stephanie Zeng <stephanie.zeng@rangle.io>
2023-02-09 15:32:52 -0500

    Add rules to install jest and update npm test script

--- schematics/src/add-lib/index.ts
+++ schematics/src/add-lib/index.ts
@@ -1,4 +1,6 @@
 import { chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
 
 const printSourceRootRule = (projectName: string) => {
   return (tree: Tree, context: SchematicContext) => {
@@ -7,7 +9,27 @@ const printSourceRootRule = (projectName: string) => {
       throw new SchematicsException('Not an Angular CLI workspace');
     }
     const projectSrcRoot = JSON.parse(workspaceConfigBuffer.toString())?.projects[projectName]?.sourceRoot;
-    context.logger.info(`printing project ${projectName} source root is ${projectSrcRoot}`);
+    context.logger.info(`📝 printing project '${projectName}' source root is '${projectSrcRoot}'`);
+    return tree;
+  };
+}
+
+const addJestToDependencyRule = () => {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask('jest'));
+    return tree;
+  };
+}
+
+const addTestScriptsRule = () => {
+  return (tree: Tree, _context: SchematicContext) => {
+    const packageJsonBuffer = tree.read('package.json');
+    if(!packageJsonBuffer) {
+      throw new SchematicsException('Package.json is not found');
+    }
+    const json = JSON.parse(packageJsonBuffer.toString());
+    json.scripts.test = 'jest';
+    tree.overwrite('package.json', JSON.stringify(json, null, 2));
     return tree;
   };
 }
@@ -15,6 +37,8 @@ const printSourceRootRule = (projectName: string) => {
 export function addLib(_options: any): Rule {
   const { project } = _options;
   return chain([
-    printSourceRootRule(project)
+    printSourceRootRule(project),
+    addTestScriptsRule(),
+    addJestToDependencyRule(),
   ]);
 }
